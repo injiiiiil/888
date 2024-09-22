OpenRA/OpenRA

We now linkin' instead 🚗

OpenRA.Mods.Common/Traits/Harvester.cs
@@ -18,10 +18,10 @@ using OpenRA.Traits;
 
 namespace OpenRA.Mods.Common.Traits
 {
-	public class HarvesterInfo : DockClientBaseInfo, Requires<IStoresResourcesInfo>, IRulesetLoaded
+	public class HarvesterInfo : LinkClientBaseInfo, Requires<IStoresResourcesInfo>, IRulesetLoaded
 	{
-		[Desc("Docking type")]
-		public readonly BitSet<DockType> Type = new("Unload");
+		[Desc("Link type")]
+		public readonly BitSet<LinkType> Type = new("Unload");
 
 		[Desc("Cell to move to when automatically unblocking DeliveryBuilding.")]
 		public readonly CVec UnblockCell = new(0, 4);
@@ -87,7 +87,7 @@ namespace OpenRA.Mods.Common.Traits
 		}
 	}
 
-	public class Harvester : DockClientBase<HarvesterInfo>, IIssueOrder, IResolveOrder, IOrderVoice,
+	public class Harvester : LinkClientBase<HarvesterInfo>, IIssueOrder, IResolveOrder, IOrderVoice,
 		ISpeedModifier, ISync, INotifyCreated
 	{
 		Mobile mobile;
@@ -96,7 +96,7 @@ namespace OpenRA.Mods.Common.Traits
 		readonly IStoresResources[] storesResources;
 		int conditionToken = Actor.InvalidConditionToken;
 
-		public override BitSet<DockType> GetDockType => Info.Type;
+		public override BitSet<LinkType> GetLinkType => Info.Type;
 
 		[Sync]
 		int currentUnloadTicks;
@@ -124,7 +124,7 @@ namespace OpenRA.Mods.Common.Traits
 		public bool IsEmpty => storesResources.All(sr => sr.ContentsSum == 0);
 		public int Fullness => storesResources.Sum(sr => sr.ContentsSum * 100 / sr.Capacity) / storesResources.Length;
 
-		protected override bool CanDock()
+		protected override bool CanLink()
 		{
 			return !IsEmpty;
 		}
@@ -152,13 +152,13 @@ namespace OpenRA.Mods.Common.Traits
 		}
 
 		IAcceptResources acceptResources;
-		public override void OnDockStarted(Actor self, Actor hostActor, IDockHost host)
+		public override void OnLinkStarted(Actor self, Actor hostActor, ILinkHost host)
 		{
-			if (IsDockingPossible(host.GetDockType))
+			if (IsLinkingPossible(host.GetLinkType))
 				acceptResources = hostActor.TraitOrDefault<IAcceptResources>();
 		}
 
-		public override bool OnDockTick(Actor self, Actor hostActor, IDockHost host)
+		public override bool OnLinkTick(Actor self, Actor hostActor, ILinkHost host)
 		{
 			if (acceptResources == null || IsTraitDisabled)
 				return true;
@@ -186,12 +186,12 @@ namespace OpenRA.Mods.Common.Traits
 			return IsEmpty;
 		}
 
-		public override void OnDockCompleted(Actor self, Actor hostActor, IDockHost dock)
+		public override void OnLinkCompleted(Actor self, Actor hostActor, ILinkHost link)
 		{
 			acceptResources = null;
 
-			// After having docked at a refinery make sure we are running FindAndDeliverResources activity.
-			if (GetDockType.Overlaps(dock.GetDockType))
+			// After having linked to a refinery make sure we are running FindAndDeliverResources activity.
+			if (GetLinkType.Overlaps(link.GetLinkType))
 			{
 				var currentActivity = self.CurrentActivity;
 				if (currentActivity == null || (currentActivity is not FindAndDeliverResources && currentActivity.NextActivity == null))

OpenRA.Mods.Common/Traits/LinkClientBase.cs
@@ -1,0 +1,54 @@
+#region Copyright & License Information
+/*
+ * Copyright (c) The OpenRA Developers and Contributors
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using OpenRA.Primitives;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Common.Traits
+{
+	public abstract class LinkClientBaseInfo : ConditionalTraitInfo, ILinkClientInfo, Requires<ILinkClientManagerInfo> { }
+
+	public abstract class LinkClientBase<InfoType> : ConditionalTrait<InfoType>, ILinkClient, INotifyCreated where InfoType : LinkClientBaseInfo
+	{
+		readonly Actor self;
+
+		public abstract BitSet<LinkType> GetLinkType { get; }
+		public LinkClientManager LinkClientManager { get; }
+
+		protected LinkClientBase(Actor self, InfoType info)
+			: base(info)
+		{
+			this.self = self;
+			LinkClientManager = self.TraitOrDefault<LinkClientManager>();
+		}
+
+		protected virtual bool CanLink()
+		{
+			return true;
+		}
+
+		public virtual bool IsLinkingPossible(BitSet<LinkType> type, bool forceEnter = false)
+		{
+			return !IsTraitDisabled && GetLinkType.Overlaps(type) && (forceEnter || CanLink());
+		}
+
+		public virtual bool CanLinkTo(Actor hostActor, ILinkHost host, bool forceEnter = false, bool ignoreOccupancy = false)
+		{
+			return (forceEnter || self.Owner.IsAlliedWith(hostActor.Owner)) && IsLinkingPossible(host.GetLinkType, forceEnter) && host.IsLinkingPossible(self, this, ignoreOccupancy);
+		}
+
+		public virtual void OnLinkStarted(Actor self, Actor hostActor, ILinkHost host) { }
+
+		public virtual bool OnLinkTick(Actor self, Actor hostActor, ILinkHost host) { return false; }
+
+		public virtual void OnLinkCompleted(Actor self, Actor hostActor, ILinkHost host) { }
+	}
+}

OpenRA.Mods.Common/TraitsInterfaces.cs
@@ -156,14 +156,14 @@ namespace OpenRA.Mods.Common.Traits
 	}
 
 	[RequireExplicitImplementation]
-	public interface INotifyDockHost { void Docked(Actor self, Actor client); void Undocked(Actor self, Actor client); }
+	public interface INotifyLinkHost { void Linked(Actor self, Actor client); void Unlinked(Actor self, Actor client); }
 	[RequireExplicitImplementation]
-	public interface INotifyDockClient { void Docked(Actor self, Actor host); void Undocked(Actor self, Actor host); }
+	public interface INotifyLinkClient { void Linked(Actor self, Actor host); void Unlinked(Actor self, Actor host); }
 
 	[RequireExplicitImplementation]
-	public interface INotifyDockClientMoving
+	public interface INotifyLinkClientMoving
 	{
-		void MovingToDock(Actor self, Actor hostActor, IDockHost host);
+		void MovingToHost(Actor self, Actor hostActor, ILinkHost host);
 		void MovementCancelled(Actor self);
 	}
 
@@ -207,75 +207,75 @@ namespace OpenRA.Mods.Common.Traits
 		void MovementCancelled(Actor self);
 	}
 
-	public interface IDockClientInfo : ITraitInfoInterface { }
+	public interface ILinkClientInfo : ITraitInfoInterface { }
 
-	public interface IDockClient
+	public interface ILinkClient
 	{
-		BitSet<DockType> GetDockType { get; }
+		BitSet<LinkType> GetLinkType { get; }
 
 		/// <summary>When null, the client should act as if it can dock but never do.</summary>
-		DockClientManager DockClientManager { get; }
-		void OnDockStarted(Actor self, Actor hostActor, IDockHost host);
-		bool OnDockTick(Actor self, Actor hostActor, IDockHost dock);
-		void OnDockCompleted(Actor self, Actor hostActor, IDockHost host);
+		LinkClientManager LinkClientManager { get; }
+		void OnLinkStarted(Actor self, Actor hostActor, ILinkHost host);
+		bool OnLinkTick(Actor self, Actor hostActor, ILinkHost dock);
+		void OnLinkCompleted(Actor self, Actor hostActor, ILinkHost host);
 
 		/// <summary>Is this client allowed to dock.</summary>
 		/// <remarks>
-		/// Does not check if <see cref="Traits.DockClientManager"/> is enabled.
-		/// Function should only be called from within <see cref="IDockClient"/> or <see cref="Traits.DockClientManager"/>.
+		/// Does not check if <see cref="Traits.LinkClientManager"/> is enabled.
+		/// Function should only be called from within <see cref="ILinkClient"/> or <see cref="Traits.LinkClientManager"/>.
 		/// </remarks>
-		bool IsDockingPossible(BitSet<DockType> type, bool forceEnter = false);
+		bool IsLinkingPossible(BitSet<LinkType> type, bool forceEnter = false);
 
 		/// <summary>Is this client allowed to dock to <paramref name="host"/>.</summary>
 		/// <remarks>
-		/// Does not check if <see cref="Traits.DockClientManager"/> is enabled.
-		/// Function should only be called from within <see cref="IDockClient"/> or <see cref="Traits.DockClientManager"/>.
+		/// Does not check if <see cref="Traits.LinkClientManager"/> is enabled.
+		/// Function should only be called from within <see cref="ILinkClient"/> or <see cref="Traits.LinkClientManager"/>.
 		/// </remarks>
-		bool CanDockAt(Actor hostActor, IDockHost host, bool forceEnter = false, bool ignoreOccupancy = false);
+		bool CanLinkTo(Actor hostActor, ILinkHost host, bool forceEnter = false, bool ignoreOccupancy = false);
 	}
 
-	public interface IDockHostInfo : ITraitInfoInterface { }
+	public interface ILinkHostInfo : ITraitInfoInterface { }
 
-	public interface IDockHost
+	public interface ILinkHost
 	{
-		BitSet<DockType> GetDockType { get; }
+		BitSet<LinkType> GetLinkType { get; }
 
 		/// <summary>Use this function instead of ConditionalTrait.IsTraitDisabled.</summary>
 		bool IsEnabledAndInWorld { get; }
 		int ReservationCount { get; }
 		bool CanBeReserved { get; }
-		WPos DockPosition { get; }
-		int DockWait { get; }
-		WAngle DockAngle { get; }
+		WPos LinkPosition { get; }
+		int LinkWait { get; }
+		WAngle LinkFacing { get; }
 
-		/// <summary>Can this <paramref name="client"/> dock at this <see cref="IDockHost"/>.</summary>
+		/// <summary>Can this <paramref name="client"/> link at this <see cref="ILinkHost"/>.</summary>
 		/// <remarks>
-		/// Does not check <see cref="DockType"/>.
-		/// Does not check if <see cref="IDockClient"/> is enabled.
-		/// Does not check if <see cref="DockClientManager"/> is enabled.
+		/// Does not check <see cref="LinkType"/>.
+		/// Does not check if <see cref="ILinkClient"/> is enabled.
+		/// Does not check if <see cref="LinkClientManager"/> is enabled.
 		/// </remarks>
-		bool IsDockingPossible(Actor clientActor, IDockClient client, bool ignoreReservations = false);
-		bool Reserve(Actor self, DockClientManager client);
+		bool IsLinkingPossible(Actor clientActor, ILinkClient client, bool ignoreReservations = false);
+		bool Reserve(Actor self, LinkClientManager client);
 		void UnreserveAll();
-		void Unreserve(DockClientManager client);
-		void OnDockStarted(Actor self, Actor clientActor, DockClientManager client);
-		void OnDockCompleted(Actor self, Actor clientActor, DockClientManager client);
+		void Unreserve(LinkClientManager client);
+		void OnLinkStarted(Actor self, Actor clientActor, LinkClientManager client);
+		void OnLinkCompleted(Actor self, Actor clientActor, LinkClientManager client);
 
-		/// <summary>If <paramref name="client"/> is not in range of <see cref="IDockHost"/> queues a child move activity and returns true. If in range returns false.</summary>
-		bool QueueMoveActivity(Activity moveToDockActivity, Actor self, Actor clientActor, DockClientManager client);
+		/// <summary>If <paramref name="client"/> is not in range of <see cref="ILinkHost"/> queues a child move activity and returns true. If in range returns false.</summary>
+		bool QueueMoveActivity(Activity moveToLinkHostActivity, Actor self, Actor clientActor, LinkClientManager client);
 
-		/// <summary>Should be called when in range of <see cref="IDockHost"/>.</summary>
-		void QueueDockActivity(Activity moveToDockActivity, Actor self, Actor clientActor, DockClientManager client);
+		/// <summary>Should be called when in range of <see cref="ILinkHost"/>.</summary>
+		void QueueLinkActivity(Activity moveToHostActivity, Actor self, Actor clientActor, LinkClientManager client);
 	}
 
-	public interface IDockHostDrag
+	public interface ILinkHostDrag
 	{
 		bool IsDragRequired { get; }
 		WVec DragOffset { get; }
 		int DragLength { get; }
 	}
 
-	public interface IDockClientManagerInfo : ITraitInfoInterface { }
+	public interface ILinkClientManagerInfo : ITraitInfoInterface { }
 
 	[RequireExplicitImplementation]
 	public interface INotifyLoadCargo

injiiiiil/888

OpenRA.Mods.Common/Traits/Harvester.cs
@@ -18,10 +18,10 @@ using OpenRA.Traits;
 
 namespace OpenRA.Mods.Common.Traits
 {
-	public class HarvesterInfo : DockClientBaseInfo, Requires<IStoresResourcesInfo>, IRulesetLoaded
+	public class HarvesterInfo : LinkClientBaseInfo, Requires<IStoresResourcesInfo>, IRulesetLoaded
 	{
-		[Desc("Docking type")]
-		public readonly BitSet<DockType> Type = new("Unload");
+		[Desc("Link type")]
+		public readonly BitSet<LinkType> Type = new("Unload");
 
 		[Desc("Cell to move to when automatically unblocking DeliveryBuilding.")]
 		public readonly CVec UnblockCell = new(0, 4);
@@ -87,7 +87,7 @@ namespace OpenRA.Mods.Common.Traits
 		}
 	}
 
-	public class Harvester : DockClientBase<HarvesterInfo>, IIssueOrder, IResolveOrder, IOrderVoice,
+	public class Harvester : LinkClientBase<HarvesterInfo>, IIssueOrder, IResolveOrder, IOrderVoice,
 		ISpeedModifier, ISync, INotifyCreated
 	{
 		Mobile mobile;
@@ -96,7 +96,7 @@ namespace OpenRA.Mods.Common.Traits
 		readonly IStoresResources[] storesResources;
 		int conditionToken = Actor.InvalidConditionToken;
 
-		public override BitSet<DockType> GetDockType => Info.Type;
+		public override BitSet<LinkType> LinkType => Info.Type;
 
 		[Sync]
 		int currentUnloadTicks;
@@ -124,7 +124,7 @@ namespace OpenRA.Mods.Common.Traits
 		public bool IsEmpty => storesResources.All(sr => sr.ContentsSum == 0);
 		public int Fullness => storesResources.Sum(sr => sr.ContentsSum * 100 / sr.Capacity) / storesResources.Length;
 
-		protected override bool CanDock()
+		protected override bool CanLink()
 		{
 			return !IsEmpty;
 		}
@@ -152,13 +152,13 @@ namespace OpenRA.Mods.Common.Traits
 		}
 
 		IAcceptResources acceptResources;
-		public override void OnDockStarted(Actor self, Actor hostActor, IDockHost host)
+		public override void OnLinkCreated(Actor self, Actor hostActor, ILinkHost host)
 		{
-			if (IsDockingPossible(host.GetDockType))
+			if (CanLink(host.GetLinkType))
 				acceptResources = hostActor.TraitOrDefault<IAcceptResources>();
 		}
 
-		public override bool OnDockTick(Actor self, Actor hostActor, IDockHost host)
+		public override bool OnLinkTick(Actor self, Actor hostActor, ILinkHost host)
 		{
 			if (acceptResources == null || IsTraitDisabled)
 				return true;
@@ -186,12 +186,12 @@ namespace OpenRA.Mods.Common.Traits
 			return IsEmpty;
 		}
 
-		public override void OnDockCompleted(Actor self, Actor hostActor, IDockHost dock)
+		public override void OnLinkRemoved(Actor self, Actor hostActor, ILinkHost link)
 		{
 			acceptResources = null;
 
-			// After having docked at a refinery make sure we are running FindAndDeliverResources activity.
-			if (GetDockType.Overlaps(dock.GetDockType))
+			// After having linked to a refinery make sure we are running FindAndDeliverResources activity.
+			if (LinkType.Overlaps(link.GetLinkType))
 			{
 				var currentActivity = self.CurrentActivity;
 				if (currentActivity == null || (currentActivity is not FindAndDeliverResources && currentActivity.NextActivity == null))

OpenRA.Mods.Common/Traits/LinkClientBase.cs
@@ -1,0 +1,56 @@
+#region Copyright & License Information
+/*
+ * Copyright (c) The OpenRA Developers and Contributors
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using OpenRA.Primitives;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Common.Traits
+{
+	public abstract class LinkClientBaseInfo : ConditionalTraitInfo, ILinkClientInfo, Requires<ILinkClientManagerInfo> { }
+
+	public abstract class LinkClientBase<InfoType> : ConditionalTrait<InfoType>, ILinkClient, INotifyCreated where InfoType : LinkClientBaseInfo
+	{
+		readonly Actor self;
+
+		public abstract BitSet<LinkType> LinkType { get; }
+		public LinkClientManager LinkClientManager { get; }
+
+		protected LinkClientBase(Actor self, InfoType info)
+			: base(info)
+		{
+			this.self = self;
+			LinkClientManager = self.TraitOrDefault<LinkClientManager>();
+		}
+
+		protected virtual bool CanLink()
+		{
+			return true;
+		}
+
+		public virtual bool CanLink(BitSet<LinkType> type, bool forceEnter = false)
+		{
+			return !IsTraitDisabled && LinkType.Overlaps(type) && (forceEnter || CanLink());
+		}
+
+		public virtual bool CanLinkTo(Actor hostActor, ILinkHost host, bool forceEnter = false, bool ignoreOccupancy = false)
+		{
+			return (forceEnter || self.Owner.IsAlliedWith(hostActor.Owner))
+				&& CanLink(host.GetLinkType, forceEnter)
+				&& host.CanLink(self, this, ignoreOccupancy);
+		}
+
+		public virtual void OnLinkCreated(Actor self, Actor hostActor, ILinkHost host) { }
+
+		public virtual bool OnLinkTick(Actor self, Actor hostActor, ILinkHost host) { return false; }
+
+		public virtual void OnLinkRemoved(Actor self, Actor hostActor, ILinkHost host) { }
+	}
+}

OpenRA.Mods.Common/TraitsInterfaces.cs
@@ -157,14 +157,14 @@ namespace OpenRA.Mods.Common.Traits
 	}
 
 	[RequireExplicitImplementation]
-	public interface INotifyDockHost { void Docked(Actor self, Actor client); void Undocked(Actor self, Actor client); }
+	public interface INotifyLinkHost { void Linked(Actor self, Actor client); void Unlinked(Actor self, Actor client); }
 	[RequireExplicitImplementation]
-	public interface INotifyDockClient { void Docked(Actor self, Actor host); void Undocked(Actor self, Actor host); }
+	public interface INotifyLinkClient { void Linked(Actor self, Actor host); void Unlinked(Actor self, Actor host); }
 
 	[RequireExplicitImplementation]
-	public interface INotifyDockClientMoving
+	public interface INotifyLinkClientMoving
 	{
-		void MovingToDock(Actor self, Actor hostActor, IDockHost host);
+		void MovingToHost(Actor self, Actor hostActor, ILinkHost host);
 		void MovementCancelled(Actor self);
 	}
 
@@ -208,75 +208,75 @@ namespace OpenRA.Mods.Common.Traits
 		void MovementCancelled(Actor self);
 	}
 
-	public interface IDockClientInfo : ITraitInfoInterface { }
+	public interface ILinkClientInfo : ITraitInfoInterface { }
 
-	public interface IDockClient
+	public interface ILinkClient
 	{
-		BitSet<DockType> GetDockType { get; }
+		BitSet<LinkType> LinkType { get; }
 
 		/// <summary>When null, the client should act as if it can dock but never do.</summary>
-		DockClientManager DockClientManager { get; }
-		void OnDockStarted(Actor self, Actor hostActor, IDockHost host);
-		bool OnDockTick(Actor self, Actor hostActor, IDockHost dock);
-		void OnDockCompleted(Actor self, Actor hostActor, IDockHost host);
+		LinkClientManager LinkClientManager { get; }
+		void OnLinkCreated(Actor self, Actor hostActor, ILinkHost host);
+		bool OnLinkTick(Actor self, Actor hostActor, ILinkHost dock);
+		void OnLinkRemoved(Actor self, Actor hostActor, ILinkHost host);
 
 		/// <summary>Is this client allowed to dock.</summary>
 		/// <remarks>
-		/// Does not check if <see cref="Traits.DockClientManager"/> is enabled.
-		/// Function should only be called from within <see cref="IDockClient"/> or <see cref="Traits.DockClientManager"/>.
+		/// Does not check if <see cref="Traits.LinkClientManager"/> is enabled.
+		/// Function should only be called from within <see cref="ILinkClient"/> or <see cref="Traits.LinkClientManager"/>.
 		/// </remarks>
-		bool IsDockingPossible(BitSet<DockType> type, bool forceEnter = false);
+		bool CanLink(BitSet<LinkType> type, bool forceEnter = false);
 
 		/// <summary>Is this client allowed to dock to <paramref name="host"/>.</summary>
 		/// <remarks>
-		/// Does not check if <see cref="Traits.DockClientManager"/> is enabled.
-		/// Function should only be called from within <see cref="IDockClient"/> or <see cref="Traits.DockClientManager"/>.
+		/// Does not check if <see cref="Traits.LinkClientManager"/> is enabled.
+		/// Function should only be called from within <see cref="ILinkClient"/> or <see cref="Traits.LinkClientManager"/>.
 		/// </remarks>
-		bool CanDockAt(Actor hostActor, IDockHost host, bool forceEnter = false, bool ignoreOccupancy = false);
+		bool CanLinkTo(Actor hostActor, ILinkHost host, bool forceEnter = false, bool ignoreOccupancy = false);
 	}
 
-	public interface IDockHostInfo : ITraitInfoInterface { }
+	public interface ILinkHostInfo : ITraitInfoInterface { }
 
-	public interface IDockHost
+	public interface ILinkHost
 	{
-		BitSet<DockType> GetDockType { get; }
+		BitSet<LinkType> GetLinkType { get; }
 
 		/// <summary>Use this function instead of ConditionalTrait.IsTraitDisabled.</summary>
 		bool IsEnabledAndInWorld { get; }
 		int ReservationCount { get; }
 		bool CanBeReserved { get; }
-		WPos DockPosition { get; }
-		int DockWait { get; }
-		WAngle DockAngle { get; }
+		WPos LinkPosition { get; }
+		int LinkWait { get; }
+		WAngle LinkFacing { get; }
 
-		/// <summary>Can this <paramref name="client"/> dock at this <see cref="IDockHost"/>.</summary>
+		/// <summary>Can this <paramref name="client"/> link at this <see cref="ILinkHost"/>.</summary>
 		/// <remarks>
-		/// Does not check <see cref="DockType"/>.
-		/// Does not check if <see cref="IDockClient"/> is enabled.
-		/// Does not check if <see cref="DockClientManager"/> is enabled.
+		/// Does not check <see cref="LinkType"/>.
+		/// Does not check if <see cref="ILinkClient"/> is enabled.
+		/// Does not check if <see cref="LinkClientManager"/> is enabled.
 		/// </remarks>
-		bool IsDockingPossible(Actor clientActor, IDockClient client, bool ignoreReservations = false);
-		bool Reserve(Actor self, DockClientManager client);
+		bool CanLink(Actor clientActor, ILinkClient client, bool ignoreReservations = false);
+		bool Reserve(Actor self, LinkClientManager client);
 		void UnreserveAll();
-		void Unreserve(DockClientManager client);
-		void OnDockStarted(Actor self, Actor clientActor, DockClientManager client);
-		void OnDockCompleted(Actor self, Actor clientActor, DockClientManager client);
+		void Unreserve(LinkClientManager client);
+		void OnLinkCreated(Actor self, Actor clientActor, LinkClientManager client);
+		void OnLinkRemoved(Actor self, Actor clientActor, LinkClientManager client);
 
-		/// <summary>If <paramref name="client"/> is not in range of <see cref="IDockHost"/> queues a child move activity and returns true. If in range returns false.</summary>
-		bool QueueMoveActivity(Activity moveToDockActivity, Actor self, Actor clientActor, DockClientManager client, MoveCooldownHelper moveCooldownHelper);
+		/// <summary>If <paramref name="client"/> is not in range of <see cref="ILinkHost"/> queues a child move activity and returns true. If in range returns false.</summary>
+		bool QueueMoveActivity(Activity moveToLinkHostActivity, Actor self, Actor clientActor, LinkClientManager client, MoveCooldownHelper moveCooldownHelper);
 
-		/// <summary>Should be called when in range of <see cref="IDockHost"/>.</summary>
-		void QueueDockActivity(Activity moveToDockActivity, Actor self, Actor clientActor, DockClientManager client);
+		/// <summary>Should be called when in range of <see cref="ILinkHost"/>.</summary>
+		void QueueLinkActivity(Activity moveToHostActivity, Actor self, Actor clientActor, LinkClientManager client);
 	}
 
-	public interface IDockHostDrag
+	public interface ILinkHostDrag
 	{
 		bool IsDragRequired { get; }
 		WVec DragOffset { get; }
 		int DragLength { get; }
 	}
 
-	public interface IDockClientManagerInfo : ITraitInfoInterface { }
+	public interface ILinkClientManagerInfo : ITraitInfoInterface { }
 
 	[RequireExplicitImplementation]
 	public interface INotifyLoadCargo